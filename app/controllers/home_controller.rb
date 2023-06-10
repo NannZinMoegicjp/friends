654ade0e663625b753decf85b521f13636290d55
nann zin moe <nannzin3@gmail.com>
2023-06-10 10:30:53 +0630

    passing parms from controller to view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
   def index
   end
   def about
-    @about_me = "My Name is John"    
+    @about_me = "My Name is John"   
+    @answer = 2 * 3 
   end
 end
